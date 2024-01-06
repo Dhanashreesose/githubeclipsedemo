8ff01e886d59bd9de6a8972f4943bebfc832a7fa
Dhanashreesose <gitedhanus25@gmail.com>
2024-01-06 13:21:15 +0530

    hi

---
+++ Sharewitheclipse1/src/main/java/com/example/demo/Sharewitheclipse1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sharewitheclipse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sharewitheclipse1Application.class, args);
+	}
+
+}
